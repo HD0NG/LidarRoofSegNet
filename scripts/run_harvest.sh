f5101185c495c6be2c57a37a28d02132ae43ec8a
HD0NG <h.dong-2@outlook.com>
2026-06-10 02:01:23 +0300

    Refactor harvest configuration to use a bash script for K-fold processing

---
+++ scripts/run_harvest.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+export MKL_THREADING_LAYER=GNU
+for k in 0 1 2 3 4; do
+  /home/ubuntu/miniconda3/envs/LiDARML/bin/python \
+    scripts/harvest_fold.py --fold $k \
+    --epochs 50 --output-dir artifacts/merge_classifier
+done
